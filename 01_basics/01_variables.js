5f84beff34a041c5481c09101ece7ee68b169776
Mansimaavi <150451832+Mansimaavi@users.noreply.github.com>
2025-05-28 07:17:54 +0000

    config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14453
+let accountEmail = "mansi@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId=2
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"
+//null is a stand alone value
+//undefine is value which is not define
+//symbol use to find the uniquness
+let name="mansi"
+let age=21
+let isLoggedIn=true
+let state=null
+
+console.log(typeof null); //object type
+console.log(typeof undefined) //undefined type
