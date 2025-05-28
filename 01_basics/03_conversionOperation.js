283ad1dd0c926979ad46eea9ae119049511023cb
Mansimaavi <150451832+Mansimaavi@users.noreply.github.com>
2025-05-28 11:11:03 +0000

    conersionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score=33
+console.log(typeof score);
+console.log(typeof (score))
+
+//33=>33(after conversion it gives back the number)
+//33abc =>NAN(after conversion it does not gives the number back)
+//true=>1;false=>0(values are converted to 0 and 1)
+let someNumber=33
+
+let stringNumber=string(soemNumber)
